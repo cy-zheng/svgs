4025a7829a856ce3537989863827adba66754ce3
zhengchenyu <chenyu.zheng@hulu.com>
2021-01-18 12:50:40 +0800

    Update oom-test.py

--- oom-test.py
+++ oom-test.py
@@ -1,6 +1,6 @@
 FILE_NAME = "/calabash/test/chenyu.zheng/10169019_RADS1012092000052026.ts"
 
-with open("/calabash/MezzanineArchive/563/138042563/10169019_RADS1012092000052026.ts", "r") as f:
+with open(FILE_NAME, "r") as f:
     while True:
         a = f.read(4096)
         if a == "":
